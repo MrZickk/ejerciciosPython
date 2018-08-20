db0916def77c0dc49a0c7dc239441630d4574530
mrzickk <mrzickk@gmail.com>
2018-08-20 12:56:25 -0500

    identacion

--- proyectoEscuela.py
+++ proyectoEscuela.py
@@ -4,99 +4,99 @@
 #el alumno se agrega desde el grupo
 
 class Grupo():
-	def __init__(self, nombreG, salon, cupo):
-		self.nombreG=nombreG
-		self.salon=salon
-		self.cupo=cupo
-		self.espacio=True
-		self.duplicado=False
-		self.duplicadoAlumno=False
-		self.materias=[]
-		self.alumnos=[]
+    def __init__(self, nombreG, salon, cupo):
+        self.nombreG=nombreG
+        self.salon=salon
+        self.cupo=cupo
+        self.espacio=True
+        self.duplicado=False
+        self.duplicadoAlumno=False
+        self.materias=[]
+        self.alumnos=[]
 
-	def __str__(self):
-		str_id = "Grupo: {0} \nSalon: {1} \nMaterias: {2} \nAlumnos: {3} \nCupo disponible: {4}"
-		return str_id.format(self.nombreG, self.salon, ", ".join([asignatura.nomAsig for asignatura in self.materias]), len(self.alumnos), self.cupo-len(self.alumnos))
-	
-	def insertar_materia(self, materia):
-		self.__verificar_duplicados(materia)
-		if self.duplicado==False:
-			self.materias.append(materia)
-			print('Materia:', str(materia), 'inscrita')
-		else:
-			print('La materia: ',str(materia),'ya se encuentra inscrita')
+    def __str__(self):
+        str_id = "Grupo: {0} \nSalon: {1} \nMaterias: {2} \nAlumnos: {3} \nCupo disponible: {4}"
+        return str_id.format(self.nombreG, self.salon, ", ".join([asignatura.nomAsig for asignatura in self.materias]), len(self.alumnos), self.cupo-len(self.alumnos))
+    
+    def insertar_materia(self, materia):
+        self.__verificar_duplicados(materia)
+        if self.duplicado==False:
+            self.materias.append(materia)
+            print('Materia:', str(materia), 'inscrita')
+        else:
+            print('La materia: ',str(materia),'ya se encuentra inscrita')
 
-	def eliminar_materia(self, materia):
-			self.materias.remove(materia)
-			print('Materia;', str(materia), 'removida')
+    def eliminar_materia(self, materia):
+            self.materias.remove(materia)
+            print('Materia;', str(materia), 'removida')
 
-	def __verificar_duplicados(self, materia):
-		tmp=materia
-		for i in self.materias:
-			if i == tmp:
-				self.duplicado=True
-			else:
-				self.duplicado=False
+    def __verificar_duplicados(self, materia):
+        tmp=materia
+        for i in self.materias:
+            if i == tmp:
+                self.duplicado=True
+            else:
+                self.duplicado=False
 
-	#valida el cupo y agrega al nuevo alumno
-	def insertar_alumno(self, alumno):
-		self.verificar_cupo()
-		self.__verificar_alumnos(alumno)
-		if self.espacio==True and self.duplicadoAlumno==False:
-			self.alumnos.append(alumno)
-			print('alumno:',str(alumno), 'agregado')
-		else:
-			print("El alumno:",str(alumno), "no se ha incrito")
+    #valida el cupo y agrega al nuevo alumno
+    def insertar_alumno(self, alumno):
+        self.verificar_cupo()
+        self.__verificar_alumnos(alumno)
+        if self.espacio==True and self.duplicadoAlumno==False:
+            self.alumnos.append(alumno)
+            print('alumno:',str(alumno), 'agregado')
+        else:
+            print("El alumno:",str(alumno), "no se ha incrito")
 
-	def eliminar_alumno(self, alumno):
-		self.alumnos.remove(alumno)
-		print('alumno',str(alumno), 'removido')
+    def eliminar_alumno(self, alumno):
+        self.alumnos.remove(alumno)
+        print('alumno',str(alumno), 'removido')
 
-	def imprimir_alumnos(self):
-		for i in self.alumnos:
-			print(i)
+    def imprimir_alumnos(self):
+        for i in self.alumnos:
+            print(i)
 
-	def __verificar_alumnos(self, alumno):
-		tmp=alumno
-		for i in self.alumnos:
-			if i ==tmp:
-				self.duplicadoAlumno=True
-			else:
-				self.duplicadoAlumno=False
+    def __verificar_alumnos(self, alumno):
+        tmp=alumno
+        for i in self.alumnos:
+            if i ==tmp:
+                self.duplicadoAlumno=True
+            else:
+                self.duplicadoAlumno=False
 
-	#metodo para validar el cupo en el grupo
-	def verificar_cupo(self):
-		cup=self.cupo
-		if cup>len(self.alumnos):
-			self.espacio=True
-			tmp=cup-len(self.alumnos)
-			print("Espacio disponible para: ", tmp, "alumnos")
-		else: 
-			self.espacio=False
+    #metodo para validar el cupo en el grupo
+    def verificar_cupo(self):
+        cup=self.cupo
+        if cup>len(self.alumnos):
+            self.espacio=True
+            tmp=cup-len(self.alumnos)
+            print("Espacio disponible para: ", tmp, "alumnos")
+        else: 
+            self.espacio=False
 
-	def modificar_cupo(self, cupo):
-		tmp=self.cupo
-		self.cupo=cupo
-		print('se ha modificado el cupo del grupo de:', tmp, 'a', self.cupo)
+    def modificar_cupo(self, cupo):
+        tmp=self.cupo
+        self.cupo=cupo
+        print('se ha modificado el cupo del grupo de:', tmp, 'a', self.cupo)
 
 
 class Asignatura():
 
-	def __init__(self, nomAsig):
-		self.nomAsig = nomAsig
+    def __init__(self, nomAsig):
+        self.nomAsig = nomAsig
 
-	def __str__(self):
-		return self.nomAsig
+    def __str__(self):
+        return self.nomAsig
 
 
 class Alumno():
-	def __init__(self, nombre, edad, matricula):
-		self.nombre=nombre
-		self.edad=edad
-		self.matricula=matricula
+    def __init__(self, nombre, edad, matricula):
+        self.nombre=nombre
+        self.edad=edad
+        self.matricula=matricula
 
-	def __str__(self):
-		return self.nombre
+    def __str__(self):
+        return self.nombre
 
 
 #------------pruebas-------------
@@ -127,7 +127,7 @@ poder cambiar el cupo
 funcion imprimir  str solo num y cupo 
 como ejecutar pruebas en python
 
-	code review
+    code review
 tab != tienen que ser 4 espacios
 nombres de metodos separeados con _
 atribuitos nombres con minusculas
